b2def5253a1135e97959915e8f3a3709680ab7d9
Arthur Friço Grillo <arthurfgrillo@gmail.com>
2024-06-24 10:59:50 -0300

    Homework change

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World! Django is running!")
+    return HttpResponse("Arthur Friço Grillo: Atividade de Cloud Computing & Site Reliability Engineering")
